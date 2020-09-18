09938270309e52a532833eec712ac7e744f53ccc
haoxian <haoxianhan@gmail.com>
2020-09-18 15:49:33 +0800

    fmt

--- src/erlpkcs7.erl
+++ src/erlpkcs7.erl
@@ -5,8 +5,8 @@
 -export ([
           pad/1,
           unpad/1,
-		  get_pad_binary/1,
-		  test/0
+          get_pad_binary/1,
+          test/0
          ]).
 
 pad(<<>>) ->
@@ -16,34 +16,34 @@ pad(Bin) ->
     pad(Bin, 16-Diff).
 
 pad(Bin, PadAmount) ->
-	PadBinary = get_pad_binary(PadAmount),
-	<<Bin/binary, PadBinary/binary>>.
+    PadBinary = get_pad_binary(PadAmount),
+    <<Bin/binary, PadBinary/binary>>.
 
 unpad(<<>>) ->
-	<<>>;
+    <<>>;
 unpad(Bin) ->
-	Last = binary:last(Bin),
-	Size = byte_size(Bin) - Last,
-	RemSize = Size rem 16,
-	PadAmount = 16 - RemSize,
-	PadBinary = get_pad_binary(PadAmount),
-	case Bin of
-		<<Data:Size/binary, PadBinaryX/binary>> when PadBinaryX == PadBinary ->
-			Data;
-		_ ->
-			erlang:error(bad_padding)
-	end.
+    Last = binary:last(Bin),
+    Size = byte_size(Bin) - Last,
+    RemSize = Size rem 16,
+    PadAmount = 16 - RemSize,
+    PadBinary = get_pad_binary(PadAmount),
+    case Bin of
+        <<Data:Size/binary, PadBinaryX/binary>> when PadBinaryX == PadBinary ->
+            Data;
+        _ ->
+            erlang:error(bad_padding)
+    end.
 
 get_pad_binary(PadAmount) ->
-	gen_pad_binary(PadAmount).
+    gen_pad_binary(PadAmount).
 
-% same as list_to_binary([Bin | lists:duplicate(PadAmount, <<PadAmount>>)]).
+%% same as list_to_binary([Bin | lists:duplicate(PadAmount, <<PadAmount>>)]).
 gen_pad_binary(PadAmount) ->
-	{parse_gen, parse_gen_padding_fun, {16}}.
+    {parse_gen, parse_gen_padding_fun, {16}}.
 
 test() ->
-	?assertEqual(pad(<<1, 1, 1, 1>>), <<1, 1, 1, 1, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12>>),
-	?assertEqual(pad(<<1, 1, 1, 1, 1, 1, 1, 1>>), <<1, 1, 1, 1, 1, 1, 1, 1, 8, 8, 8, 8, 8, 8, 8, 8>>),
-	?assertEqual(pad(<<1, 1, 1, 1>>), <<1, 1, 1, 1, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12>>),
-	?assertEqual(unpad(<<1, 1, 1, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13>>), <<1, 1, 1>>),
-	?assertEqual(unpad(<<1, 1, 1, 1, 1, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11>>), <<1, 1, 1, 1, 1>>).
+    ?assertEqual(pad(<<1, 1, 1, 1>>), <<1, 1, 1, 1, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12>>),
+    ?assertEqual(pad(<<1, 1, 1, 1, 1, 1, 1, 1>>), <<1, 1, 1, 1, 1, 1, 1, 1, 8, 8, 8, 8, 8, 8, 8, 8>>),
+    ?assertEqual(pad(<<1, 1, 1, 1>>), <<1, 1, 1, 1, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12>>),
+    ?assertEqual(unpad(<<1, 1, 1, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13>>), <<1, 1, 1>>),
+    ?assertEqual(unpad(<<1, 1, 1, 1, 1, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11>>), <<1, 1, 1, 1, 1>>).
