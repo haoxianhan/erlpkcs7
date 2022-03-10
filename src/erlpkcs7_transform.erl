d627533d7a54d362a432ed4f37696f967616f965
haoxian <haoxianhan@gmail.com>
2022-03-10 11:42:02 +0800

    format

--- src/erlpkcs7_transform.erl
+++ src/erlpkcs7_transform.erl
@@ -9,27 +9,33 @@ parse_transform(AST, _Options) ->
 
 walk_ast(Acc, []) ->
     lists:reverse(Acc);
-walk_ast(Acc, [{function, Line, Name, Arity, Clauses}|T]) ->
-    walk_ast([{function, Line, Name, Arity, walk_clauses([], Clauses)}|Acc], T);
-walk_ast(Acc, [H|L]) ->
-    walk_ast([H|Acc], L).
+walk_ast(Acc, [{function, Line, Name, Arity, Clauses} | T]) ->
+    walk_ast([{function, Line, Name, Arity, walk_clauses([], Clauses)} | Acc], T);
+walk_ast(Acc, [H | L]) ->
+    walk_ast([H | Acc], L).
 
 walk_clauses(Acc, []) ->
     lists:reverse(Acc);
-walk_clauses(Acc, [{clause, Line, Arguments, Guards, [{tuple,_,
-                                                       [{atom,_,parse_gen},
-                                                        {atom,_,ParseFun},
-                                                        Argus]}]} | T]) ->
+walk_clauses(Acc, [
+    {clause, Line, Arguments, Guards, [
+        {tuple, _, [
+            {atom, _, parse_gen},
+            {atom, _, ParseFun},
+            Argus
+        ]}
+    ]}
+    | T
+]) ->
     %% re-loop for excute walk_body
     AddClause = ?MODULE:ParseFun(clause, Line, Arguments, Guards, Argus),
     walk_clauses(Acc, AddClause ++ T);
-walk_clauses(Acc, [{clause, Line, Arguments, Guards, Body}|T]) ->
-    walk_clauses([{clause, Line, Arguments, Guards, walk_body([], Body)}|Acc], T).
+walk_clauses(Acc, [{clause, Line, Arguments, Guards, Body} | T]) ->
+    walk_clauses([{clause, Line, Arguments, Guards, walk_body([], Body)} | Acc], T).
 
 walk_body(Acc, []) ->
     lists:reverse(Acc);
-walk_body(Acc, [H|T]) ->
-    walk_body([transform_statement(H)|Acc], T).
+walk_body(Acc, [H | T]) ->
+    walk_body([transform_statement(H) | Acc], T).
 
 transform_statement(Stmt) when is_tuple(Stmt) ->
     list_to_tuple(transform_statement(tuple_to_list(Stmt)));
@@ -39,8 +45,10 @@ transform_statement(Stmt) ->
     Stmt.
 
 parse_gen_padding_fun(clause, Line, _Arguments, _Guards, {tuple, _, [{integer, _, PaddingAmount}]}) ->
-    [{clause, Line, [{integer, Line, X}], [],
-      [{bin,Line,
-        lists:duplicate(X, {bin_element,Line,{integer,Line,X},default,default})
-       }]
-     } || X <- lists:seq(1, PaddingAmount)].
+    [
+        {clause, Line, [{integer, Line, X}], [], [
+            {bin, Line,
+                lists:duplicate(X, {bin_element, Line, {integer, Line, X}, default, default})}
+        ]}
+     || X <- lists:seq(1, PaddingAmount)
+    ].
